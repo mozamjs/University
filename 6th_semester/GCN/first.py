f1509d58e36f7e5b0fd716ab8be62114fa3e7377
mozamjs <mozamqurexhi38@gmail.com>
2026-09-03 17:38:08 +0500

    6th semester add

---
+++ 6th_semester/GCN/first.py
@@ -0,0 +1,51 @@
+# arr = [10,20,30,40,50]
+# print(arr)
+
+# arr[1]= 55
+# print(arr)
+
+# arr.append(49)
+# print(arr)
+
+# arr.remove(40)
+# print(arr)
+
+# print(len(arr))
+
+# str = ["musab","mozam","mohihb"]
+# print(str)
+
+#07 Taking input from user
+
+
+# from array import *
+# val = array('i' , [])
+
+# n = int(input('Enter number of Elements in the array :'))
+
+# for i in range(0,n):
+#     val.append(int(input("Enter next number :")))
+
+# for x in val:
+#     print(x, end=" ")
+
+
+
+# Type Casting 
+# from array import *
+
+# val = array([1,2,3,4,5,6, 7, 8.5], float)
+# for i in val:
+#     print(i)
+
+
+#10 arithmatic progression
+from numpy import *
+
+val = linspace(10,20,5)
+for i in val:
+    print(i)
+
+val = arange(10,20,2)
+for i in val:
+    print(i)
---
+++ 6th_semester/GCN/loop.py
@@ -0,0 +1,5 @@
+
+arr = [12,7,13,4,15]
+
+for i in arr:
+    print(i)
