d6e4bebec7fa96c85440221c873ad1fbb9d05d4d
mozamjs <mozamqurexhi38@gmail.com>
2026-09-17 19:16:12 +0500

    chsnge

---
+++ 6th_semester/GCN/graph.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt 
+import networkx as nx
+
+G1 = nx.cycle_graph(8)
+
+plt.figure(figsize=(8,6))
+
+nx.draw(G1,
+        with_labels=True,
+labels
+        node_color='lightblue',
+        node_size=500,
+        font_size=14,
+font_size 
+        font_weight='bold'
+
+labels
+        font_color='darkred' )
